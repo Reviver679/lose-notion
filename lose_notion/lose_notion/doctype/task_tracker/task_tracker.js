c4bbf20c8e3e21de5a33d9496dc88317a7ae105d
Keshav <keshavsabu@gmail.com>
2026-01-25 08:14:17 +0000

    auto save after insert on task tracker .js

--- lose_notion/lose_notion/doctype/task_tracker/task_tracker.js
+++ lose_notion/lose_notion/doctype/task_tracker/task_tracker.js
@@ -1,8 +1,47 @@
-// Copyright (c) 2026, alfaEdge and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Task Tracker', {
+    onload: function(frm) {
+        frm.auto_save_timeout = null;
+    }
+});
 
-// frappe.ui.form.on("Task Tracker", {
-// 	refresh(frm) {
+frappe.ui.form.on('Task Tracker Table', {
+    task_name: function(frm, cdt, cdn) {
+        trigger_auto_save(frm);
+    },
+    
+    assigned_to: function(frm, cdt, cdn) {
+        trigger_auto_save(frm);
+    },
+    
+    status: function(frm, cdt, cdn) {
+        trigger_auto_save(frm);
+    },
+    
+    deadline: function(frm, cdt, cdn) {
+        trigger_auto_save(frm);
+    },
+    
+    task_tracker_table_add: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (!row.status) {
+            frappe.model.set_value(cdt, cdn, 'status', 'Not Started');
+        }
+    }
+});
 
-// 	},
-// });
+function trigger_auto_save(frm) {
+    if (frm.auto_save_timeout) {
+        clearTimeout(frm.auto_save_timeout);
+    }
+    
+    frm.auto_save_timeout = setTimeout(() => {
+        if (frm.is_dirty()) {
+            frm.save('Save', null, null, () => {
+                frappe.show_alert({
+                    message: __('Tasks auto-saved'),
+                    indicator: 'green'
+                }, 2);
+            });
+        }
+    }, 800);
+}
