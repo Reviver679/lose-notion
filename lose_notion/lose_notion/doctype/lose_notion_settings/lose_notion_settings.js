905beaad4bc025a27b60dc831df998c02d862923
Keshav <keshavsabu@gmail.com>
2026-02-02 10:41:12 +0000

    created lose notion settings

---
+++ lose_notion/lose_notion/doctype/lose_notion_settings/lose_notion_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, alfaEdge and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Lose Notion Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
